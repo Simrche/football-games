26cb65c772cdb48e895b3cfdb0ad9b9f9e5aec03
Simrche <simon.roche@edu.devinci.fr>
2023-10-26 15:34:46 +0200

    add code iso

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,57 @@
 <template>
-    <div class="flex flex-col mt-12 gap-8 justify-center items-center">
-        <div
-            v-for="player in players"
-            :key="player.id"
-            class="border flex bg-gray-100 border-gray-300 rounded-4xl h-28 px-4 w-4/10 items-center justify-around hover:shadow-lg"
-        >
-            <div class="rounded-full bg-gray-300 h-16 w-16">
-                <img
-                    class="rounded-full"
-                    :src="player.photo"
-                    :alt="player.fullname"
-                    :title="player.fullname"
-                />
+    <div class="flex flex-col py-12 gap-6 justify-center items-center">
+        <div v-for="player in players" :key="player.id" class="w-4/10">
+            <div class="font-bold text-center text-lg w-full">
+                {{ player.fullname }}
             </div>
             <div
-                class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
+                class="border flex bg-gray-100 border-gray-300 rounded-4xl h-28 mt-2 px-4 items-center justify-around hover:shadow-lg"
             >
-                {{ player.nationality.toUpperCase().slice(0, 2) }}
-            </div>
-            <div
-                class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
-            >
-                {{ player.age }}
-            </div>
-            <div
-                class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
-            >
-                <img
-                    class="rounded-full w-2/4"
-                    :src="player.league_photo"
-                    :alt="player.league_name"
-                    :title="player.league_name"
-                />
-            </div>
-            <div
-                class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
-            >
-                {{ positionDict[player.position] }}
+                <div class="rounded-full bg-gray-300 h-16 w-16">
+                    <img
+                        class="rounded-full"
+                        :src="player.photo"
+                        :alt="player.fullname"
+                        :title="player.fullname"
+                    />
+                </div>
+                <div
+                    class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
+                >
+                    <img
+                        class="rounded-full w-1/2"
+                        :src="player.league_photo"
+                        :alt="player.league_name"
+                        :title="player.league_name"
+                    />
+                </div>
+                <div
+                    class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
+                >
+                    <img
+                        class="w-1/2"
+                        :src="player.team_photo"
+                        :alt="player.team_name"
+                        :title="player.team_name"
+                    />
+                </div>
+                <div
+                    class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
+                >
+                    <img
+                        class="w-1/2"
+                        :src="`https://media-4.api-sports.io/flags/${
+                            iso3311a2.getCode(player.nationality) ??
+                            nationalityDict[player.nationality]
+                        }.svg`"
+                        alt=""
+                    />
+                </div>
+                <div
+                    class="rounded-full flex font-bold bg-gray-300 h-16 w-16 items-center justify-center"
+                >
+                    {{ positionDict[player.position] }}
+                </div>
             </div>
         </div>
     </div>
@@ -44,13 +59,12 @@
 
 <script lang="ts" setup>
 import { onMounted, ref } from "@nuxtjs/composition-api";
+import * as iso3311a2 from "iso-3166-1-alpha-2";
 import { uniqBy } from "lodash";
 import { useSupabase } from "~/utils";
 import { Player } from "~/utils/types";
 
 const supabase = useSupabase();
-
-console.log(supabase);
 
 const players = ref<Player[]>([]);
 
@@ -82,5 +96,10 @@ const positionDict: Record<string, string> = {
     Midfielder: "MF",
     Defender: "DF",
     Goalkeeper: "GK",
+};
+
+const nationalityDict: Record<string, string> = {
+    England: "GB",
+    "Korea Republic": "KR",
 };
 </script>
