549e48dff7e5988dd5f99ca8f058f7e58207af02
Nvcq <nicolas.mopin@gmail.com>
2023-11-13 15:28:03 +0100

    index style

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <h1>Welcome to Football games !</h1>
-        <p>Try football Wordle</p>
+    <div class="flex flex-col justify-center w-screen">
+        <h1 class="text-center mt-28 text-3xl font-bold">Welcome to Football games !</h1>
+        <NuxtLink to="/wordle" class="text-center mt-28 text-lg hover:underline cursor-default">Try football Wordle</NuxtLink>
     </div>
 </template>
