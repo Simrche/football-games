49968a1f786e2de0e01e5dc7c4044173dfdf743b
Simrche <simon.roche@edu.devinci.fr>
2023-11-13 23:07:06 +0100

    add nuxtlink to logo name

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="flex flex-col justify-center w-screen">
-        <h1 class="text-center mt-28 text-3xl font-bold">Welcome to Football games !</h1>
-        <NuxtLink to="/wordle" class="text-center mt-28 text-lg hover:underline cursor-default">Try football Wordle</NuxtLink>
+        <h1 class="text-center mt-28 text-3xl font-bold">
+            Welcome to Football games !
+        </h1>
+        <NuxtLink
+            to="/wordle"
+            class="text-center mt-28 text-lg hover:underline cursor-default"
+            >Try football Wordle</NuxtLink
+        >
+        <NuxtLink
+            to="/logo"
+            class="text-center mt-28 text-lg hover:underline cursor-default"
+            >Try football Logos</NuxtLink
+        >
     </div>
 </template>
