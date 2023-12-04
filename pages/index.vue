86ef9a8b41c8e2d45b7f879bb6e741fbcf057f44
Simrche <simon.roche@edu.devinci.fr>
2023-12-04 22:48:20 +0100

    add button's shadow

--- pages/index.vue
+++ pages/index.vue
@@ -30,17 +30,17 @@
             </div>
             <div class="flex mt-4 gap-x-2">
                 <div
-                    class="border-black rounded-md cursor-pointer flex border-[4px] h-10 w-10 items-center justify-center hover:bg-green-800 hover:text-white"
+                    class="border-black rounded-md cursor-pointer flex border-[4px] h-10 shadow-lg w-10 items-center justify-center hover:bg-green-800 hover:text-white"
                 >
                     <i :class="`mdi mdi-weather-night`" class="text-xl"></i>
                 </div>
                 <div
-                    class="border-black rounded-md cursor-pointer flex border-[4px] h-10 w-10 items-center justify-center hover:bg-green-800 hover:text-white"
+                    class="border-black rounded-md cursor-pointer flex border-[4px] h-10 shadow-lg w-10 items-center justify-center hover:bg-green-800 hover:text-white"
                 >
                     <i :class="`mdi mdi-cog`" class="text-xl"></i>
                 </div>
                 <div
-                    class="border-black rounded-md cursor-pointer flex border-[4px] h-10 w-10 items-center justify-center hover:bg-green-800 hover:text-white"
+                    class="border-black rounded-md cursor-pointer flex border-[4px] h-10 shadow-lg w-10 items-center justify-center hover:bg-green-800 hover:text-white"
                 >
                     <i :class="`mdi mdi-book`" class="text-xl"></i>
                 </div>
